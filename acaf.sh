3de4317fb23fb2f164e6202cfe637f8be8a02cec
Ibrahim AlTamimi <ibm.iloom@gmail.com>
2020-02-10 23:59:31 +0200

    fix build file

--- acaf.sh
+++
@@ -1,5 +0,0 @@
-./gradlew clean build install
-
-export BINTRAY_USER=ibm-iloom
-export BINTRAY_KEY=20b56d9f26acaddc7c635b8c5f7e71af48cd9f44
-./gradlew :clean assembleRelease :fragmentation:bintrayUpload
